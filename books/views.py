99dc29e0400b69dda30a85bd6ee59d16eab773ae
Viktor Rocha <viktoorrocha@gmail.com>
2025-07-10 10:42:20 -0400

    feat: Estrutura inicial da API (modelos, serializadores, visualizações, roteamento)
    Define modelos de Autor e Livro, cria ModelViewSets, configura serializadores e configura o roteamento DRF. Inclui Swagger UI para documentação.

--- books/views.py
+++ books/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+# books/views.py 
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Book, Author
+from .serializers import AuthorSerializer, BookSerializer
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint que permite que os autores sejam visualizados ou editados.
+    """
+    queryset = Author.objects.all().order_by('name')
+    serializer_class = AuthorSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint que permite que os livros sejam visualizados ou editados.
+    """
+    queryset = Book.objects.all().order_by('title')
+    serializer_class = BookSerializer
